68cd5e9d9c80ca27c274bedff81a3e15681a8ef6
Revati Natu <revatinatu@hotmail.com>
2025-06-30 23:48:07 +0530

    hand_tracking basic camera on set

---
+++ Hand_Tracking/hand_tracking.py
@@ -0,0 +1,17 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+while True:
+    success, img = cap.read()
+    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
