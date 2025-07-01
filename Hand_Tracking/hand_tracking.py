0327d3578eac805b1bf6fb0c6c1ef944e9395623
Revati Natu <revatinatu@hotmail.com>
2025-07-01 22:28:25 +0530

    Basic hand tracking

--- Hand_Tracking/hand_tracking.py
+++ Hand_Tracking/hand_tracking.py
@@ -7,11 +7,18 @@ cap = cv2.VideoCapture(0) # Use 0 for the default camera, or replace with extern
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
     
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)  
     
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
