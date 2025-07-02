244d4faaaf06f906d41a1a600a198e2c91a0c2d6
Revati Natu <revatinatu@hotmail.com>
2025-07-02 23:40:21 +0530

    hand point tracking

--- Hand_Tracking/hand_tracking.py
+++ Hand_Tracking/hand_tracking.py
@@ -9,6 +9,9 @@ hands = mpHands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_trackin
 
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
     
@@ -18,7 +21,20 @@ while True:
     
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+                # if id == 0: # Index of the wrist landmark - U can change this to any landmark you want to track
+                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 4)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
